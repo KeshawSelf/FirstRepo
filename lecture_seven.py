782495fa6a5810f513248414b80d7a49e99fd759
PocoSelf <heykeshawself@gmail.com>
2026-07-26 13:41:33 +0530

    CS50p

--- lecture_seven.py
+++ lecture_seven.py
@@ -1,1 +1,49 @@
+# name = input("What's your name? ").strip().title()
+# first, second, third = name.split()
+# print(f"hello, {first}")
+# print(f"hello, {second}")
+# print(f"hello, {third}")
+
+# x = float(input("What's x? "))
+# y = float(input("What's y? "))
+# # print(type(x + y))
+# # print(int(input("What's x? ")) + int(input("What's y? ")))
+# z = x + y
+
+# print(f"{z:.0f}")
+# def hello(name):
+#     print(f"hello, {name}")
+# x = input("What's your name? ")
+# hello(x)
+
+# def main() :
+#     name = input("What's your name? ")
+#     hello(name)
+#     hello()
+
+# def hello(to="world"):
+#     print("hello, " ,to)
+
+# main()
+
+# def main():
+#     n = int(input("Enter a number: "))
+#     print("Square is",square(n))
+
+
+# def square(x):
+#     return x * x
+
+# main()
+
+# a = "keshaw anand"
+# b = a.casefold()
+# c = input("Enter: ")
+# d = c.casefold().strip()
+# if (b == d) :
+#     print("yes")
+# else:
+#     print("No")
+if(2 > 1):
+    print("yes")
 
