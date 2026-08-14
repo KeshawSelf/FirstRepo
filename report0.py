ed2cb676974fa55fecd4e9bbe865961bea4da83e
PocoSelf <heykeshawself@gmail.com>
2026-08-14 19:27:57 +0530

    cs50p

--- report0.py
+++ report0.py
@@ -119,22 +119,22 @@
 
 # main()
 
-distances = {
-    "Voyager 1": 163,
-    "Voyager 2": 136,
-    "Pioneer 10": 80,
-    "New Horizons": 58,
-    "Pioneer 11": 44,
-}
+# distances = {
+#     "Voyager 1": 163,
+#     "Voyager 2": 136,
+#     "Pioneer 10": 80,
+#     "New Horizons": 58,
+#     "Pioneer 11": 44,
+# }
 
 
-def main():
-    for distance in distances.values():
-        print(f"{distance} AU is {convert(distance)} m")
+# def main():
+#     for distance in distances.values():
+#         print(f"{distance} AU is {convert(distance)} m")
 
 
-def convert(au):
-    return au * 149597870700
+# def convert(au):
+#     return au * 149597870700
 
 
-main()
+# main()
