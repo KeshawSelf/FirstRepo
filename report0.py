db440dff259d89c47e25a64aac769d6caf9c075b
PocoSelf <heykeshawself@gmail.com>
2026-08-14 10:11:57 +0530

    cs50p

---
+++ report0.py
@@ -0,0 +1,140 @@
+# def main():
+#    ...
+
+# def create_report(spacecraft):
+#   return f"""
+#      ========= REPORT =========
+#      Name: {spacecraft["name"]}
+#      Distance: {spacecraft["distance"]} AU
+
+#     ==========================
+#     """
+# main()
+# def main():
+#     spacecraft = {"name": "Voyager 1", "distance": 163}
+#     print(create_report(spacecraft))
+
+
+# def create_report(spacecraft):
+#     return f"""
+#     ========= REPORT =========
+
+#     Name: {spacecraft["name"]}
+#     Distance: {spacecraft["distance"]} AU
+
+#     ==========================
+#     """
+
+
+# main()
+
+# def main():
+#     spacecraft = {"name": "James Webb Space Telescope"}
+#     print(create_report(spacecraft))
+
+
+# def create_report(spacecraft):
+#     return f"""
+#     ========= REPORT =========
+
+#     Name: {spacecraft["name"]}
+#     Distance: {spacecraft["distance"]} AU
+
+#     ==========================
+#     """
+
+
+# main()
+
+# def main():
+#     spacecraft = {"name": "James Webb Space Telescope"}
+#     print(create_report(spacecraft))
+
+
+# def create_report(spacecraft):
+#     return f"""
+#     ========= REPORT =========
+
+#     Name: {spacecraft.get("name", "Unknown")}
+#     Distance: {spacecraft.get("distance", "Unknown")} AU
+
+#     ==========================
+#     """
+
+
+# main()
+
+# def main():
+#     spacecraft = {"name": "James Webb Space Telescope"}
+#     spacecraft["distance"] = 0.01
+#     print(create_report(spacecraft))
+
+
+# def create_report(spacecraft):
+#     return f"""
+#     ========= REPORT =========
+
+#     Name: {spacecraft.get("name", "Unknown")}
+#     Distance: {spacecraft.get("distance", "Unknown")} AU
+
+#     ==========================
+#     """
+
+
+# main()
+
+# def main():
+#     spacecraft = {"name": "James Webb Space Telescope"}
+#     spacecraft.update({"distance": 0.01, "orbit": "Sun"})
+#     print(create_report(spacecraft))
+
+
+# def create_report(spacecraft):
+#     return f"""
+#     ========= REPORT =========
+
+#     Name: {spacecraft.get("name", "Unknown")}
+#     Distance: {spacecraft.get("distance", "Unknown")} AU
+#     Orbit: {spacecraft.get("orbit", "Unknown")}
+
+#     ==========================
+#     """
+
+
+# main()
+
+# distances = {
+#     "Voyager 1": 163,
+#     "Voyager 2": 136,
+#     "Pioneer 10": 80,
+#     "New Horizons": 58,
+#     "Pioneer 11": 44,
+# }
+
+
+# def main():
+#     for name in distances.keys():
+#         print(f"{name} is {distances[name]} AU from Earth")
+
+
+# main()
+
+distances = {
+    "Voyager 1": 163,
+    "Voyager 2": 136,
+    "Pioneer 10": 80,
+    "New Horizons": 58,
+    "Pioneer 11": 44,
+}
+
+
+def main():
+    for distance in distances.values():
+        print(f"{distance} AU is {convert(distance)} m")
+
+
+def convert(au):
+    return au * 149597870700
+
+
+main()
