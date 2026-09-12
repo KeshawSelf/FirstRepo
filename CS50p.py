5b9951ee8b72f7c95296f6cda97ed12105e048e9
PocoSelf <heykeshawself@gmail.com>
2026-09-12 21:39:11 +0530

    unknown

--- CS50p.py
+++ CS50p.py
@@ -315,3 +315,4 @@ except ValueError:
     print("x is not an integer")
 else:
     print(f"x is {x}")
+    
---
+++ trying.py
@@ -0,0 +1,7 @@
+# x = float(input("Enter: "))
+# print(f"you entered: {x:.1f}")
+# a = "hello, there"
+# if a.endswith("there"):
+#     print("true")
+# else:
+#     print("false")
