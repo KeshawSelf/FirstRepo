71dd39b0d121bcf17f50652ea2de882e189384e4
PocoSelf <heykeshawself@gmail.com>
2026-09-22 08:39:11 +0530

    done

--- trying.py
+++ trying.py
@@ -5,3 +5,12 @@
 #     print("true")
 # else:
 #     print("false")
+def main():
+    height = int(input("Height: "))
+    pyramid(height)
+
+def pyramid(n):
+    for i in range(1,n+1):
+        print("#" * i)
+
+main()
